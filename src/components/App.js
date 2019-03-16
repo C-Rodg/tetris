3953fc2fcf2c94f202356619dbfdbc0f16fc5ff3
C-Rodg <C-Rodg@users.noreply.github.com>
2019-03-16 15:16:52 -0700

    Setup dashboard for controls

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 
 // Components
 import Board from './Board';
+import Dashboard from './Dashboard';
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Board />
+				<Dashboard />
 			</div>
 		);
 	}
---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Dashboard = () => {
+	return (
+		<div className="Dashboard">
+			<div>Start Game or Quit Game</div>
+			<div>Pause</div>
+			<div>Score</div>
+			<div>Level</div>
+			<div>Lines</div>
+		</div>
+	);
+};
+
+export default Dashboard;
